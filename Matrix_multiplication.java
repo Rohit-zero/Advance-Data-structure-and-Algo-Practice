21840bf2aa943c146ec567da2acfa7f6a28dbb0a
Rohit39 <77542893+Rohit-zero@users.noreply.github.com>
2022-03-25 00:04:03 +0530

    Update Matrix_multiplication.java

--- Matrix_multiplication.java
+++ Matrix_multiplication.java
@@ -2,6 +2,7 @@ public class Matrix_multiplication {
     public void multiplication(int[][] arr1,int[][] arr2) {
         
     }
+    
     public static void main(String[] args) {
         int[][] arr1={{2,1},{4,9}};
         int[][] arr2={{4,3},{8,2}};
