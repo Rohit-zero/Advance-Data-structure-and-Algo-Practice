a8da9d0c3ed1fecbd5dd66cb51c364030f0318d0
Rohit-zero <Rohitdhaked39@gmail.com>
2022-03-27 19:04:52 +0530

    Merge branch 'master' of https://github.com/Rohit-zero/Advance-Data-structure-and-Algo-Practice

--- BackTracking/N_queen.java
+++ BackTracking/N_queen.java
@@ -1,21 +1,56 @@
 public class N_queen {
-    public void positions(int r, int c, int n) {
-        if(c==n)
+    public boolean positions(int[][] arr, int row, int n) {
+        if(row==n)
         {
-            return;
+            return true;
         }
         for(int i=0;i<n;i++)
         {
-            
+            // i is represting columns
+
+            if(check(arr,row,i))
+            {
+                arr[row][i]=1;
+                if(positions(arr,row+1,n))
+                {
+                    return true;
+                }
+                arr[row][i]=0;
+            }
         }
+        return false;
     }
 
-    public void check() {
-        
+    public boolean check(int arr[][],int row,int col) {
+        for(int i=0; i<arr.length;i++)
+        {
+            if(arr[row][i]==1)
+            {
+                return false;
+            }
+            if(arr[i][col]==1)
+            {
+                return false;
+            }
+            if(arr[i][i]==1)
+            {
+                return false;
+            }
+        }
+        return true;
     }
     public static void main(String[] args) {
-        int n=4     // size of matrix
+        int n=4;     // size of matrix
+        int[][] arr=new int[n][n];
+
+        // inserting 0 in each cell
+        // zero means queen not present and ones means queen are there
+        for(int i=0;i<n;i++)
+        {
+            arr[0][i]=0;
+            arr[i][0]=0;
+        }
         N_queen o = new N_queen();
-        o.positions(0, 0,n);
+        o.positions(arr,0,n);
     }
 }
